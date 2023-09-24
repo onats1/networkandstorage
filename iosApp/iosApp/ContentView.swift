474d0497f5a6b3e991334538a9e276728541a5c7
oluwadamilareonatemowo <oluwadamilare@flutterwavego.com>
2023-09-24 09:31:16 +0100

    ios UI implemented.

--- iosApp/iosApp/ContentView.swift
+++ iosApp/iosApp/ContentView.swift
@@ -2,15 +2,62 @@ import SwiftUI
 import shared
 
 struct ContentView: View {
-	let greet = Greeting().greet()
+    @ObservedObject private(set) var viewModel: ViewModel
 
-	var body: some View {
-		Text(greet)
-	}
+         var body: some View {
+             NavigationView {
+                 listView()
+                 .navigationBarTitle("SpaceX Launches")
+                 .navigationBarItems(trailing:
+                     Button("Reload") {
+                         self.viewModel.loadLaunches(forceReload: true)
+                 })
+             }
+         }
+
+         private func listView() -> AnyView {
+             switch viewModel.launches {
+             case .loading:
+                 return AnyView(Text("Loading...").multilineTextAlignment(.center))
+             case .result(let launches):
+                 return AnyView(List(launches) { launch in
+                     RocketLaunchRow(rocketLaunch: launch)
+                 })
+             case .error(let description):
+                 return AnyView(Text(description).multilineTextAlignment(.center))
+             }
+         }
 }
 
-struct ContentView_Previews: PreviewProvider {
-	static var previews: some View {
-		ContentView()
-	}
+extension ContentView {
+    enum LoadableLaunches {
+        case loading
+        case result([RocketLaunch])
+        case error(String)
+    }
+
+   @MainActor
+   class ViewModel: ObservableObject {
+       let sdk: SpaceXSDK
+       @Published var launches = LoadableLaunches.loading
+       
+       init(sdk: SpaceXSDK) {
+                   self.sdk = sdk
+                   self.loadLaunches(forceReload: false)
+               }
+
+               func loadLaunches(forceReload: Bool) {
+                   Task {
+                           do {
+                               self.launches = .loading
+                               let launches = try await sdk.getLaunches(forceReload: forceReload)
+                               self.launches = .result(launches)
+                           } catch {
+                               self.launches = .error(error.localizedDescription)
+                           }
+                       }
+               }
+   }
 }
+
+extension RocketLaunch: Identifiable { }
